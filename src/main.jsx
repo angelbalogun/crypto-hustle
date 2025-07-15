ef62e5aa3fbe47383d8946124f69894f2d1c5220
angelbalogun <balogunangel@yahoo.com>
2025-07-14 23:08:19 -0400

    Decsribe your changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Layout from './routes/Layout'
+import DetailView from './routes/DetailView'
+import NotFound from './routes/NotFound'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<App />}/>
+          <Route path="/coinDetails/:symbol" element={<DetailView />}/>
+          <Route path="*" element={ <NotFound /> } />
+        </Route>
+    </Routes>
+  </BrowserRouter>
 )
